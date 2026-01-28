c5684d97a2925ad0af331cfcf279eeab0c4ac4b6
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2026-01-28 15:01:36 +0100

    Chore: Update for 2xend

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const authRoute = require('./src/routes/authRoutes')
+const express = require("express");
+const cors = require("cors");
+const authRoute = require("./src/routes/authRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/auth',authRoute)
+app.use("/auth", authRoute);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });

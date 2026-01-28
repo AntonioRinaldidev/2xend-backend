f651e64cda042c866ee298cc79fab82f58345fa3
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2026-01-28 15:55:59 +0100

    Adding get /

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoute);
+app.get("/", (req, res) => {
+  res.send("2xend Backend is running");
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");

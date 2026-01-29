37bf5c5d7e7b66e792c5768b15a1e66548807bf2
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2026-01-29 09:06:17 +0100

    Fix: jwt.config to camelCase

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,33 +1,39 @@
 require("dotenv").config();
 
 const jwtConfig = {
-    accesstoken:{
-    secret : process.env.JWT_SECRET,
-    expiresIn : '15m',
-    algorithm : 'HS256',
-},
-    refreshtoken: {
-        secret : process.env.JWT_REFRESH_SECRET,
-        expiresIn : '7d',
-        algorithm : 'HS256',
-    }
-}
+  accessToken: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: "15m",
+    algorithm: "HS256",
+  },
+  refreshToken: {
+    secret: process.env.JWT_REFRESH_SECRET,
+    expiresIn: "7d",
+    algorithm: "HS256",
+  },
+};
 if (!jwtConfig.accesstoken.secret) {
-    throw new Error('JWT secret is not set in environment variables.');
+  throw new Error("JWT secret is not set in environment variables.");
 }
 if (!jwtConfig.accesstoken.expiresIn) {
-    throw new Error('JWT expiration time is not set in environment variables.');
+  throw new Error("JWT expiration time is not set in environment variables.");
 }
 if (!jwtConfig.accesstoken.algorithm) {
-    throw new Error('JWT algorithm is not set in environment variables.');
+  throw new Error("JWT algorithm is not set in environment variables.");
 }
 if (!jwtConfig.refreshtoken.secret) {
-    throw new Error('JWT refresh token secret is not set in environment variables.');
+  throw new Error(
+    "JWT refresh token secret is not set in environment variables.",
+  );
 }
 if (!jwtConfig.refreshtoken.expiresIn) {
-    throw new Error('JWT refresh token expiration time is not set in environment variables.');
+  throw new Error(
+    "JWT refresh token expiration time is not set in environment variables.",
+  );
 }
 if (!jwtConfig.refreshtoken.algorithm) {
-    throw new Error('JWT refresh token algorithm is not set in environment variables.');
+  throw new Error(
+    "JWT refresh token algorithm is not set in environment variables.",
+  );
 }
-module.exports = jwtConfig
+module.exports = jwtConfig;

8722484375d178bb58b7cc528d407967acf5c001
Antonio Rinaldi <a.rinaldi.dev@gmail.com>
2026-01-30 10:46:41 +0100

    Fix: added use custom route to  index

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const authRoute = require("./src/routes/authRoutes");
+const userRoute = require("./src/routes/userRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+
 app.get("/", (req, res) => {
   res.send("2xend Backend is running");
 });
